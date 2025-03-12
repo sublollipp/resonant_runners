f825cf4371ff6916d46326aa4f3b17f8c5f72a2f
Duirky <eriitenov@gmail.com>
2025-03-12 11:15:43 +0100

    Update segment.gd

--- Scripts/segment.gd
+++ Scripts/segment.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
-@onready var width = $MainTilemap.get_used_rect().size.x*16
 @onready var width = $MainTilemap.get_used_rect().size.x * 16
+
 var difficulty : String = "undefined";
