2694b8e12871e0023a18ebbd6446886be802e80a
ElmerHebs <110420368+ElmerHebs@users.noreply.github.com>
2025-03-17 10:46:12 +0100

    optimize catchup

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -23,18 +23,16 @@ var jumpKey : String = "Jump"
 var crouchKey : String = "Crouch"
 
 
-	
-	
 func set_as_player_two() -> void:
 	player2 = true
 	leftKey = "2Left"
 	rightKey = "2Right"
 	jumpKey = "2Jump"
 	crouchKey = "2Crouch"
-	
+
+
 func velocityPositionReset() -> void:
 	velocity.x = 0
-
 
 
 func _physics_process(delta) -> void:
@@ -46,34 +44,40 @@ func _physics_process(delta) -> void:
 	# Handle jump.
 	if ray_cast.is_colliding(): #checker ikke om den player er crouching, tilader at hoppe på andre i alle tilfælde
 		var collider = ray_cast.get_collider()
+		
 		if collider.is_in_group("players"):
-			is_on_player=true
+			is_on_player = true
+			
 			if collider.is_crouching:
 				is_on_crouching_player = true
 		else: 
 			is_on_player = false
 			is_on_crouching_player = false
+	
 			
 	if Input.is_action_just_pressed(jumpKey) and !is_crouching:
+		
 		if is_on_crouching_player:
 			velocity.y = JUMP_VELOCITY*1.5
+			
 		elif is_on_floor() and !is_on_player:
 			velocity.y = JUMP_VELOCITY
 			
-			
-	
-	
 	var direction : float = Input.get_axis(leftKey, rightKey)
 	
 	if direction:
 		print(position.x)
 		animation.play("Running")
+		
 		if direction > 0:
 			velocity.x = SPEED
+			
 		elif direction < 0:
 			velocity.x = -SPEED - Gamespeed.speed
+			
 		else:
 			velocityPositionReset()
+			
 	else:
 		animation.play("Idle")
 		velocityPositionReset()
@@ -81,6 +85,7 @@ func _physics_process(delta) -> void:
 	
 	if direction<0:
 		animation.flip_h=true
+		
 	else:
 		animation.flip_h = false
 		
@@ -89,12 +94,9 @@ func _physics_process(delta) -> void:
 		velocity.x =  -Gamespeed.speed
 		animation.play("Crouching")
 		collision_shape_2d.shape.size.y = start_collision_shape_height-5
+		
 	if Input.is_action_just_released(crouchKey):  #ineffektiv kode skal være on release istedet
 		is_crouching = false
 		collision_shape_2d.shape.size.y = start_collision_shape_height
-	
-		
-	
-		
-		
+
 	move_and_slide()
