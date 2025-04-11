110f7dd6f2bf6c51e1fecef9f64c255c87b37418
Duirky <eriitenov@gmail.com>
2025-04-11 13:57:17 +0200

    det sudste

--- Scripts/gamescript.gd
+++ Scripts/gamescript.gd
@@ -12,7 +12,6 @@ func _ready() -> void:
 	player1.position = Vector2(40, -48)
 	player1.name = "Player 1"
 
-
 	$CamController.add_child(player1)
 	GDSync.set_gdsync_owner($CamController.get_node("Player 1"), GDSync.lobby_get_all_clients()[0])
 	#if ResonantRunners.debugPC > 1:
