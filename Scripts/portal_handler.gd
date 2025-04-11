54bd7c82c4b9f6289a0bf3d6f55e011d4ae27b3c
ElmerHebs <110420368+ElmerHebs@users.noreply.github.com>
2025-04-11 09:45:02 +0200

    Portal teleporter limit fix

--- Scripts/portal_handler.gd
+++ Scripts/portal_handler.gd
@@ -1,8 +1,6 @@
 extends Node
 
 
-
-
 var inPortal : bool = false:
 	set(newVal):
 		if newVal != inPortal:
@@ -10,6 +8,13 @@ var inPortal : bool = false:
 var previousPortal : ColorGate = null
 
 @onready var player : Player = get_parent()
+@onready var camController = player.get_parent()
+@onready var rightLimit = camController.get_child(2)
+@onready var rightLimitCollision = rightLimit.get_child(0)
+
+
+func _ready():
+	pass
 
 func checkIfInPortal() -> void:
 	if not get_parent().get_node("PortalArea").has_overlapping_bodies():
@@ -17,57 +22,52 @@ func checkIfInPortal() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PortalCollider") && !inPortal:
-		print("player: ", player.velocity)
+		
 		var portal = body.get_parent() as ColorGate
-		inPortal = true
-		previousPortal = portal
 		
-		var globalPointA = portal.to_global(portal.points[0])
-		var globalPointB = portal.to_global(portal.points[1])
+		#her burde det være den protal x der er størst mod den mindste x værdi af collision shapen
 		
-		var portalDirection = (globalPointB - globalPointA).normalized()
-		var entryAngle = atan2(portalDirection.y, portalDirection.x)
-		
-		
-		#normal vektor
-		var portalNormal = Vector2(-portalDirection.y, portalDirection.x)
-		print("entry: ", rad_to_deg(entryAngle))
-		
-		#checker om retningens vektoren projekteret på portal vektoren er negativ (hvis ja så er det forskellige retning, hvis nej så er det samme)
-		
-		
-		var globalExitPointA = portal.pairedPortal.to_global(portal.pairedPortal.points[0])
-		var globalExitPointB = portal.pairedPortal.to_global(portal.pairedPortal.points[1])
-		# Compute the direction and angle from the global points
-		var exitDirection = (globalExitPointB - globalExitPointA).normalized()
-		var exitAngle = atan2(exitDirection.y, exitDirection.x)
-		
-		#detection if first portal is entered from the back (if then ad pi to angle diff)
-		if portal.pairedPortal.flipExitPortal:
-			print("entered backside")
-			entryAngle += PI
-		
-		print("exit: ", rad_to_deg(exitAngle))
-		
-		var angleDiff = exitAngle - entryAngle
-		print("angle diff: ", rad_to_deg(angleDiff))
-		
-		
-		
-		
-		# Teleporter spiller
-		player.global_position = portal.pairedPortal.global_position + (portal.pairedPortal.points[0] + (portal.pairedPortal.points[1] - portal.pairedPortal.points[0]) / 2).rotated(portal.pairedPortal.rotation)
-		
-		# rotere spiller baseret på forskellen
-		var rotatingVector = player.velocity
-		
-		if !player.is_crouching:
-			rotatingVector.x += Gamespeed.speed
+		if portal.pairedPortal.global_position.x < rightLimitCollision.global_position.x-(rightLimitCollision.shape.size.x/2):
+			inPortal = true
+			previousPortal = portal
 			
-		rotatingVector = rotatingVector.rotated(angleDiff)
-		rotatingVector.x -= Gamespeed.speed
-		player.velocity = rotatingVector
-		print("player after: ", player.velocity)
+			var globalPointA = portal.to_global(portal.points[0])
+			var globalPointB = portal.to_global(portal.points[1])
+			
+			var portalDirection = (globalPointB - globalPointA).normalized()
+			var entryAngle = atan2(portalDirection.y, portalDirection.x)
+			
+			#normal vektor
+			var portalNormal = Vector2(-portalDirection.y, portalDirection.x)
+			
+			#checker om retningens vektoren projekteret på portal vektoren er negativ (hvis ja så er det forskellige retning, hvis nej så er det samme)
+			
+			var globalExitPointA = portal.pairedPortal.to_global(portal.pairedPortal.points[0])
+			var globalExitPointB = portal.pairedPortal.to_global(portal.pairedPortal.points[1])
+			# Compute the direction and angle from the global points
+			var exitDirection = (globalExitPointB - globalExitPointA).normalized()
+			var exitAngle = atan2(exitDirection.y, exitDirection.x)
+			
+			#detection if first portal is entered from the back (if then ad pi to angle diff)
+			if portal.pairedPortal.flipExitPortal:
+				entryAngle += PI
+			
+			var angleDiff = exitAngle - entryAngle
+
+			
+			# Teleporter spiller
+			player.global_position = portal.pairedPortal.global_position + (portal.pairedPortal.points[0] + (portal.pairedPortal.points[1] - portal.pairedPortal.points[0]) / 2).rotated(portal.pairedPortal.rotation)
+			
+			# rotere spiller baseret på forskellen
+			var rotatingVector = player.velocity
+			
+			if !player.is_crouching:
+				rotatingVector.x += Gamespeed.speed
+				
+			rotatingVector = rotatingVector.rotated(angleDiff)
+			rotatingVector.x -= Gamespeed.speed
+			player.velocity = rotatingVector
+
 
 
 
