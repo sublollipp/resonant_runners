bd033b892154f111870f75fb4c68c1bfee973785
ElmerHebs <110420368+ElmerHebs@users.noreply.github.com>
2025-04-30 10:16:20 +0200

    ui igen

--- GD-SyncTemplates/LobbyBrowser/lobby_label.gd
+++ GD-SyncTemplates/LobbyBrowser/lobby_label.gd
@@ -5,14 +5,22 @@ signal join_pressed(lobby_name : String, has_password : bool)
 var has_password : bool
 
 func set_lobby_data(data : Dictionary):
+	var font = load("res://Assets/Fonts/PixelOperator8.ttf")
 	name = data["Name"]
 	has_password = data["HasPassword"]
 	%LobbyName.text = name
+	%LobbyName.add_theme_font_override("font",font)
 	%PlayerCount.text = str(data["PlayerCount"])+"/"+str(data["PlayerLimit"])
+	%PlayerCount.add_theme_font_override("font",font)
 	%PasswordProtected.text = str(has_password).capitalize()
+	%PasswordProtected.add_theme_font_override("font",font)
 	%Open.text = str(data["Open"]).capitalize()
+	%Open.add_theme_font_override("font",font)
 	
 	%JoinButton.disabled = !data["Open"]
+	
+	var buttonTheme = load("res://NormalButton.tres")
+	%JoinButton.theme = buttonTheme
 	
 	var tags : Dictionary = data["Tags"]
 	if tags.has("Gamemode"): %Gamemode.text = tags["Gamemode"]
